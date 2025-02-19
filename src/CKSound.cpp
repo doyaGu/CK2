db5690133150edb6d1f2c44d74f70f041a564a7f
doyaGu <doyaGu@users.noreply.github.com>
2025-02-18 22:37:16 -0500

    Update CKSound

--- src/CKSound.cpp
+++ src/CKSound.cpp
@@ -1,21 +1,28 @@
 #include "CKSound.h"
 
+#include "CKFile.h"
+#include "CKStateChunk.h"
+#include "CKPathManager.h"
+
+extern CKSTRING CKJustFile(CKSTRING path);
+
+CK_CLASSID CKSound::m_ClassID = CKCID_SOUND;
+
 CK_SOUND_SAVEOPTIONS CKSound::GetSaveOptions() {
-    return CKSOUND_EXTERNAL;
+    return m_SaveOptions;
 }
 
-CK_CLASSID CKSound::m_ClassID;
-
 void CKSound::SetSaveOptions(CK_SOUND_SAVEOPTIONS Options) {
-
+    m_SaveOptions = Options;
 }
 
 CKSound::CKSound(CKContext *Context, CKSTRING name) : CKBeObject(Context, name) {
-
+    m_FileName = nullptr;
+    m_SaveOptions = CKSOUND_USEGLOBAL;
 }
 
 CKSound::~CKSound() {
-
+    delete[] m_FileName;
 }
 
 CK_CLASSID CKSound::GetClassID() {
@@ -23,33 +30,69 @@ CK_CLASSID CKSound::GetClassID() {
 }
 
 CKStateChunk *CKSound::Save(CKFile *file, CKDWORD flags) {
-    return CKBeObject::Save(file, flags);
+    CKStateChunk* baseChunk = CKBeObject::Save(file, flags);
+
+    CKStateChunk* chunk = new CKStateChunk(CKCID_SOUND, file);
+
+    chunk->StartWrite();
+    chunk->AddChunkAndDelete(baseChunk);
+
+    CK_SOUND_SAVEOPTIONS saveOptions = m_SaveOptions;
+    if (saveOptions == CKSOUND_USEGLOBAL)
+        saveOptions = m_Context->m_GlobalSoundsSaveOptions;
+
+    if (file && saveOptions == CKSOUND_INCLUDEORIGINALFILE)
+        file->IncludeFile(m_FileName, SOUND_PATH_IDX);
+
+    chunk->WriteIdentifier(CK_STATESAVE_SOUNDFILENAME);
+    chunk->WriteInt(m_SaveOptions);
+    chunk->WriteString(CKJustFile(m_FileName));  // Write the file name without path
+
+    if (GetClassID() == CKCID_SOUND)
+        chunk->CloseChunk();
+    else
+        chunk->UpdateDataSize();
+    return chunk;
 }
 
 CKERROR CKSound::Load(CKStateChunk *chunk, CKFile *file) {
-    return CKBeObject::Load(chunk, file);
+    if (!chunk)
+        return CKERR_INVALIDPARAMETER;
+
+    CKBeObject::Load(chunk, file);
+
+    if (chunk->SeekIdentifier(CK_STATESAVE_SOUNDFILENAME)) {
+        delete[] m_FileName;
+        m_FileName = nullptr;
+
+        m_SaveOptions = (CK_SOUND_SAVEOPTIONS)chunk->ReadDword();
+
+        chunk->ReadString(&m_FileName);
+    }
+
+    return CK_OK;
 }
 
 int CKSound::GetMemoryOccupation() {
-    return CKBeObject::GetMemoryOccupation();
+    return CKBeObject::GetMemoryOccupation() + 8;
 }
 
-CKSTRING CKSound::GetClassNameA() {
-    return nullptr;
+CKSTRING CKSound::GetClassName() {
+    return "Sound";
 }
 
 int CKSound::GetDependenciesCount(int mode) {
-    return 0;
+    return CKObject::GetDependenciesCount(mode);
 }
 
 CKSTRING CKSound::GetDependencies(int i, int mode) {
-    return nullptr;
+    return CKObject::GetDependencies(i, mode);
 }
 
 void CKSound::Register() {
-
+    CKClassRegisterAssociatedParameter(m_ClassID, CKPGUID_SOUND);
 }
 
 CKSound *CKSound::CreateInstance(CKContext *Context) {
-    return nullptr;
+    return new CKSound(Context);
 }
