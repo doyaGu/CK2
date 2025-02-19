2e374636225caeabc0b9123b2f14715b9f6d863f
doyaGu <doyaGu@users.noreply.github.com>
2025-02-18 21:17:56 -0500

    Remove friendship declaration for CKBehaviorPrototypeTemporary in CKBehaviorPrototype

--- include/CKBehaviorPrototype.h
+++ include/CKBehaviorPrototype.h
@@ -107,7 +107,6 @@ See also: CreateCKBehaviorPrototype,CKBehavior
 class DLL_EXPORT CKBehaviorPrototype
 {
     friend class CKBehavior;
-    friend class CKBehaviorPrototypeTemporary;
 
 public:
     //---------------------------------------------------------------------
