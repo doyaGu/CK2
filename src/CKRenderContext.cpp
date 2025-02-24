4451af92a4a707190d59502052e34cccb73b7664
doyaGu <doyaGu@users.noreply.github.com>
2025-02-24 15:54:30 -0500

    Initialize CKRenderContext and update CMakeLists to include it

--- src/CKRenderContext.cpp
+++ src/CKRenderContext.cpp
@@ -1,8 +1,11 @@
 #include "CKRenderContext.h"
 
-CKRenderContext::CKRenderContext(CKContext *Context, CKSTRING name) {
+CKRenderContext::CKRenderContext(CKContext *Context, CKSTRING name) : CKObject(Context, name) {
+	if (!Context->m_BehaviorContext.CurrentRenderContext)
+		Context->m_BehaviorContext.CurrentRenderContext = this;
 }
 
 CKRenderContext::~CKRenderContext() {
-	CKObject::~CKObject();
+    if (m_Context->m_BehaviorContext.CurrentRenderContext == this)
+        m_Context->m_BehaviorContext.CurrentRenderContext = nullptr;
 }
